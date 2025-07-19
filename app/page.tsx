32ae6106f11ca1c2f8ed2b1d71a8d7512db8a37d
Cosmic AI <support@cosmicjs.com>
2025-07-19 09:50:54 -0700

    Fix TypeScript errors by fetching and passing required data props to section components

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,24 @@ import ProjectsSection from '@/components/ProjectsSection';
 import ExperienceSection from '@/components/ExperienceSection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 import ContactSection from '@/components/ContactSection';
+import { getProjects, getSkills, getWorkExperience, getTestimonials } from '@/lib/cosmic';
 
-export default function Home() {
+export default async function Home() {
+  // Fetch all data from Cosmic CMS
+  const [skills, projects, experience, testimonials] = await Promise.all([
+    getSkills(),
+    getProjects(),
+    getWorkExperience(),
+    getTestimonials()
+  ]);
+
   return (
     <div className="min-h-screen">
       <Hero />
-      <SkillsSection />
-      <ProjectsSection />
-      <ExperienceSection />
-      <TestimonialsSection />
+      <SkillsSection skills={skills} />
+      <ProjectsSection projects={projects} />
+      <ExperienceSection experience={experience} />
+      <TestimonialsSection testimonials={testimonials} />
       <ContactSection />
     </div>
   );
